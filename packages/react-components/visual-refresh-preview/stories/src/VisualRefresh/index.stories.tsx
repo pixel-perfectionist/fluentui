e8cc4451378b478aa41649b5171f0fb9bc607eda
Alexander Katrukhin <Oleksandr.Katrukhin@microsoft.com>
2025-10-22 15:19:19 -0400

    Add Button visual refresh story and component setup

---
+++ packages/react-components/visual-refresh-preview/stories/src/VisualRefresh/index.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Meta } from '@storybook/react';
+
+export { ButtonVisualRefresh as Button } from './Button.stories';
+
+const Component = () => <div />;
+
+export default {
+  title: 'Components/VisualRefresh',
+  component: Component,
+  parameters: {},
+} as Meta;
